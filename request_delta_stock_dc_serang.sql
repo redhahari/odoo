01cbf8d805cc86293ed37d978b6933ef28ed289d
redhahari <redha.hari.w@gmail.com>
2016-04-04 13:18:09 +0700

    Update request_delta_stock_dc_serang.sql

--- request_delta_stock_dc_serang.sql
+++ request_delta_stock_dc_serang.sql
@@ -1,8 +1,8 @@
--- from tanggal 28 sampai dengan 31 maret 2016
-
--- data stock DARI DC SERANG STOCK
-
-select a.nama_dagang,a.kode_item_lama,a.kode_item_baru,a.lokasi_sumber,a.last_update,a.state,
+SELECT b.nama_dagang nama_dagang_dari,b.kode_item_lama kode_item_lama_dari,b.kode_item_baru kode_item_baru_dari,b.lokasi_sumber,b.state state_dari,
+	   c.nama_dagang nama_dagang_ke ,c.kode_item_lama kode_item_lama_ke,c.kode_item_baru kode_item_baru_ke,c.lokasi_tujuan,c.state state_ke,
+	   coalesce(c.quantity,0)-coalesce(b.quantity,0) as delta_quantity
+FROM 
+(select a.nama_dagang,a.kode_item_lama,a.kode_item_baru,a.lokasi_sumber,a.state,
        sum(a.quantity) as quantity
 from
 	(select pt.name nama_dagang,
@@ -22,12 +22,12 @@ from
 	and sl1.complete_name ilike '%serang%' -- Untuk Catch Physical Locations / DC Serang / Stock
 	and sl1.complete_name ilike '%stock%' -- Untuk Catch Physical Locations / DC Serang / Stock
 	and cast(sm.write_date as date) between '2016-03-28' and '2016-03-31') a
- GROUP BY kode_item_baru,kode_item_lama,nama_dagang,lokasi_sumber,last_update,state
- ORDER BY last_update,kode_item_baru,state
+ GROUP BY kode_item_baru,kode_item_lama,nama_dagang,lokasi_sumber,state
+ ORDER BY kode_item_baru,lokasi_sumber,state) b
 
--- data stock KE DC SERANG STOCK
+FULL OUTER JOIN
 
-select a.nama_dagang,a.kode_item_lama,a.kode_item_baru,a.lokasi_tujuan,a.last_update,a.state,
+(select a.nama_dagang,a.kode_item_lama,a.kode_item_baru,a.lokasi_tujuan,a.state,
        sum(a.quantity) as quantity
 from
 	(select pt.name nama_dagang,
@@ -47,5 +47,9 @@ from
 	and sl2.complete_name ilike '%serang%' -- Untuk Catch Physical Locations / DC Serang / Stock
 	and sl2.complete_name ilike '%stock%' -- Untuk Catch Physical Locations / DC Serang / Stock
 	and cast(sm.write_date as date) between '2016-03-28' and '2016-03-31') a
- GROUP BY kode_item_baru,kode_item_lama,nama_dagang,lokasi_tujuan,last_update,state
- ORDER BY last_update,kode_item_baru,state
+ GROUP BY kode_item_baru,kode_item_lama,nama_dagang,lokasi_tujuan,state
+ ORDER BY kode_item_baru,lokasi_tujuan,state) c
+ON b.kode_item_baru = c.kode_item_baru
+and b.lokasi_sumber = c.lokasi_tujuan
+and b.state = c.state
+ORDER BY c.state,b.state desc nulls last
