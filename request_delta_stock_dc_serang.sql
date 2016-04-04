147d71e443655b4c114aed0444060ca43bc7092a
redhahari <redha.hari.w@gmail.com>
2016-04-04 12:40:56 +0700

    Update request_delta_stock_dc_serang.sql

--- request_delta_stock_dc_serang.sql
+++ request_delta_stock_dc_serang.sql
@@ -2,7 +2,7 @@
 
 -- data stock DARI DC SERANG STOCK
 
-select a.nama_dagang,a.kode_item_lama,a.kode_item_baru,a.lokasi_sumber,a.lokasi_tujuan,a.last_update,a.state,
+select a.nama_dagang,a.kode_item_lama,a.kode_item_baru,a.lokasi_sumber,a.last_update,a.state,
        sum(a.quantity) as quantity
 from
 	(select pt.name nama_dagang,
@@ -22,7 +22,7 @@ from
 	and sl1.complete_name ilike '%serang%' -- Untuk Catch Physical Locations / DC Serang / Stock
 	and sl1.complete_name ilike '%stock%' -- Untuk Catch Physical Locations / DC Serang / Stock
 	and cast(sm.write_date as date) between '2016-03-28' and '2016-03-31') a
- GROUP BY kode_item_baru,kode_item_lama,nama_dagang,lokasi_sumber,lokasi_tujuan,last_update,state
+ GROUP BY kode_item_baru,kode_item_lama,nama_dagang,lokasi_sumber,last_update,state
  ORDER BY last_update,kode_item_baru,state
 
 -- data stock KE DC SERANG STOCK
